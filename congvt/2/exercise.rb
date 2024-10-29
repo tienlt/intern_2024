f572f4bbae3ecd65889e53f7fde461e0e2f2dcbf
vtcong21 <vtcong21@vp.fitus.edu.vn>
2024-10-29 15:15:54 +0700

    congvt update exercise 2

--- congvt/2/exercise.rb
+++ congvt/2/exercise.rb
@@ -1,4 +1,5 @@
 def word_frequency(large_file_path, word)
-  regex = /\b#{word}\b/i
+  regex = /\b#{word}\b/
   File.foreach(large_file_path).sum{|line| line.scan(regex).count}
 end
+# puts word_frequency("1.txt", "ruby")
